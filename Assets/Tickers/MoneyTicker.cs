f661bb9c5294efceba030229ad1a955c3af203c6
Matthew Huxtable <matthew.huxtable@sparx.co.uk>
2019-10-19 16:01:53 +0100

    Add new generic event & tickers broadcast mech

---
+++ Assets/Tickers/MoneyTicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace mosquitodefenders.Tickers
+{
+
+    public class MoneyTicker : IResourceTicker<double>
+    {
+        private double balance;
+        private readonly double increment;
+
+        public MoneyTicker(double incrementAmount, double initialBalance = 0.0)
+        {
+            balance = initialBalance;
+            increment = incrementAmount;
+        }
+
+        public double NextValue()
+        {
+            balance += increment;
+            return balance;
+        }
+    }
+}
---
+++ Assets/Tickers/TimeTicker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mosquitodefenders.Tickers
+{
+    public class TimeTicker : IResourceTicker<DateTime>
+    {
+        public DateTime currentDate;
+
+        public TimeTicker()
+        {
+            currentDate = DateTime.Parse("01/01/2000");
+        }
+
+        public DateTime NextValue()
+        {
+            Debug.LogFormat("Update time {0}", currentDate.ToString());
+            currentDate = currentDate.AddHours(1);
+            return currentDate;
+        }
+    }
+}

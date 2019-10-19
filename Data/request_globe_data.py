20bc11195b217164d044b2ed759cfffba6361655
Jamie Conn <Jamie.Conn@sparx.co.uk>
2019-10-19 13:26:39 +0100

    Calculate utm easting and northing

--- Data/request_globe_data.py
+++ Data/request_globe_data.py
@@ -2,6 +2,7 @@ import requests as r
 import json
 import pandas as pd
 import sys
+import utm
 
 base_url = 'https://api.globe.gov/search/v1/measurement/protocol/'
 
@@ -21,10 +22,23 @@ parsedData = []
 
 for obs in data['features']:
   parsedObs = obs['properties']
-  parsedObs['latCoord'] = obs['geometry']['coordinates'][0]
-  parsedObs['longCoord'] = obs['geometry']['coordinates'][1]
-  parsedData.append(parsedObs)
+  
+  latitude = obs['geometry']['coordinates'][1]
+  longitude = obs['geometry']['coordinates'][0]
+  
+  try:
+    easting, northing, zoneNum, zoneLetter = utm.from_latlon(latitude, longitude)
+  except:
+    print("skipping: ", longitude, latitude)
+    continue
 
+  parsedObs['latitude'] = latitude
+  parsedObs['longitude'] = longitude
+  parsedObs['easting'] = easting
+  parsedObs['northing'] = northing
+
+  parsedData.append(parsedObs)
 
 df = pd.DataFrame(parsedData)
 df.to_csv('data.csv')
+

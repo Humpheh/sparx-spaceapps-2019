27b3a7d2c0e4a3189bbac77454076fcd7576e53d
Jamie Conn <Jamie.Conn@sparx.co.uk>
2019-10-19 17:44:59 +0100

    Flask app now samples from positive survey locations

--- Data/flask_app.py
+++ Data/flask_app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from json import dumps
+import json
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
+with open('processed/survey_data.json') as json_file:
+    data = json.load(json_file)
+
 class Events(Resource):
     def get(self):
-        return {'event': {'lat': 50, 'long': 50}}
-
-        
+        rand_int = random.randint(0, len(data))
+        lat = data[rand_int]['latitude']
+        long = data[rand_int]['longitude']
+        events = [{'event': {'lat': lat, 'long': long, 'type': 1, 'severity': 3, 'text': 'Reports of 23 people diagnosed with Malaria'}}]
+        return(events)
 
 api.add_resource(Events, '/events')
 

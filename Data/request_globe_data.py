5a2f27cb926a7b11881fb554fe9a55721ff18c00
Jamie Conn <Jamie.Conn@sparx.co.uk>
2019-10-19 22:49:00 +0100

    Change timer to severity

--- Data/request_globe_data.py
+++ Data/request_globe_data.py
@@ -126,23 +126,23 @@ def generate_text(row):
     text += "."
     return(text)
     
-def generate_timer(row):
+def generate_severity(row):
     if row['adults']:
-        return(15)
+        return('adults')
     if row['pupae']:
-        return(30)
-    return(60) 
+        return('pupae')
+    return('eggs') 
 
 
 gdf['text'] = gdf.apply(generate_text, axis=1)
-gdf['timer'] = gdf.apply(generate_timer, axis=1)
+gdf['severity'] = gdf.apply(generate_severity, axis=1)
 
 gdf = gdf[[
   'latitude',
   'longitude',
   'text',
   'image_url',
-  'timer',
+  'severity',
 ]]
 
 

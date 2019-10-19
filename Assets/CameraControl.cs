f3a3c3f94d43a747c79d2806cd3e6cd416118e7a
Humphrey Shotton <humphrey.shotton@sparx.co.uk>
2019-10-19 11:27:11 +0100

    Initial commit!

---
+++ Assets/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Vector3 velocity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = new Vector3();
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y -= 1;
+        }
+
+        transform.position = transform.position + diff.normalized * Time.deltaTime;
+    }
+}

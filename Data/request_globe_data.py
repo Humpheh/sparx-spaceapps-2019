e822440620e8ed8230d8849650a46aea6a5adf95
Scott Thomas <scott.thomas@sparx.co.uk>
2019-10-19 20:46:21 +0100

    process mosquito data cols

--- Data/request_globe_data.py
+++ Data/request_globe_data.py
@@ -33,3 +33,67 @@ for obs in data['features']:
 with open('processed/mosquito_data.json', 'w') as ff:
   json.dump(parsedData, ff)
 
+# pass through geodataframe format and keep only key columns
+
+gdf = gpd.GeoDataFrame(
+    parsedData, 
+    geometry = gpd.points_from_xy(df.longitude, df.latitude),
+    crs = {'init' :'epsg:4326'}
+).rename(columns = {
+    "countryName": "country",
+    "mosquitohabitatmapperMeasuredAt": "timestamp",
+    "mosquitohabitatmapperWaterSource": "waterSource",
+    "mosquitohabitatmapperWaterSourceType": "sourceType",
+    "mosquitohabitatmapperLarvaeCount": "larvaeCount",
+    "mosquitohabitatmapperMosquitoAdults": "adults",
+    "mosquitohabitatmapperMosquitoEggs": "eggs",
+    "mosquitohabitatmapperMosquitoPupae": "pupae",
+    "mosquitohabitatmapperGenus": "genus",
+    "mosquitohabitatmapperSpecies": "species",
+    "mosquitohabitatmapperLarvaFullBodyPhotoUrls": "larvae_images",
+    "mosquitohabitatmapperAbdomenCloseupPhotoUrls": "abdomen_images",
+    "mosquitohabitatmapperWaterSourcePhotoUrls": "water_images",
+}).drop(columns = {
+    "mosquitohabitatmapperMeasurementLongitude", 
+    "mosquitohabitatmapperMeasurementLatitude",
+    "countryCode",
+    "mosquitohabitatmapperBreedingGroundEliminated",
+    "elevation",
+    "mosquitohabitatmapperDataSource",
+    "mosquitohabitatmapperMeasurementElevation",
+    "mosquitohabitatmapperMosquitoHabitatMapperId",
+    "mosquitohabitatmapperUserid",
+    "organizationId",
+    "organizationName",
+    "protocol",
+    "siteId",
+    "siteName",
+    "mosquitohabitatmapperLastIdentifyStage",
+    "mosquitohabitatmapperComments",
+    "geometry"
+})
+
+def coalesce(df, column_names):
+    i = iter(column_names)
+    column_name = next(i)
+    answer = df[column_name]
+    for column_name in i:
+        answer = answer.fillna(df[column_name])
+    return answer
+
+def first_image_only(series):
+    newvar = []
+    for (i, photos) in enumerate(series):
+        if np.isreal(photos) and np.isnan(photos):
+            newvar.append("") # no image
+        else:
+            newvar.append(photos.split(";")[0]) # first listed image
+    return newvar
+
+gdf['image_name'] = coalesce(gdf, ["abdomen_images", "larvae_images", "water_images"])
+gdf['image_name'] = first_image_only(gdf.image_name)
+
+gdf = gdf.drop(
+    columns = {"abdomen_images", "larvae_images", "water_images"}
+)
+

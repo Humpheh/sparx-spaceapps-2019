59411c190a522afced71deeb62e2fd2cbbac14e3
Jamie Conn <Jamie.Conn@sparx.co.uk>
2019-10-19 18:41:06 +0100

    Improve flask app

--- Data/flask_app.py
+++ Data/flask_app.py
@@ -7,14 +7,26 @@ app = Flask(__name__)
 api = Api(app)
 
 with open('processed/survey_data.json') as json_file:
-    data = json.load(json_file)
+    survey_data = json.load(json_file)
+
+
+with open('processed/mosquito_data.json') as json_file:
+    mosquito_data = json.load(json_file)
 
 class Events(Resource):
     def get(self):
-        rand_int = random.randint(0, len(data))
-        lat = data[rand_int]['latitude']
-        long = data[rand_int]['longitude']
-        events = [{'event': {'lat': lat, 'long': long, 'type': 1, 'severity': 3, 'text': 'Reports of 23 people diagnosed with Malaria'}}]
+        rand_int = random.randint(0, 1)
+        if rand_int == 0:
+            rand_int = random.randint(0, len(survey_data))
+            lat = survey_data[rand_int]['latitude']
+            long = survey_data[rand_int]['longitude']
+            text = 'Diagnosis'
+        if rand_int == 1:
+            rand_int = random.randint(0, len(mosquito_data))
+            lat = mosquito_data[rand_int]['latitude']
+            long = mosquito_data[rand_int]['longitude']
+            text = 'Mosquito sighting'
+        events = [{'event': {'lat': lat, 'long': long, 'type': 1, 'severity': 3, 'text': text}}]
         return(events)
 
 api.add_resource(Events, '/events')
--- Data/request_globe_data.py
+++ Data/request_globe_data.py
@@ -2,7 +2,6 @@ import requests as r
 import json
 import pandas as pd
 import sys
-import utm
 
 base_url = 'https://api.globe.gov/search/v1/measurement/protocol/'
 
@@ -21,28 +20,16 @@ data = res.json()
 parsedData = []
 
 for obs in data['features']:
-  parsedObs = obs['properties']
-
+  #parsedObs = obs['properties']
+  parsedObs = {}  
   latitude = obs['geometry']['coordinates'][1]
   longitude = obs['geometry']['coordinates'][0]
-
-  try:
-    easting, northing, zoneNum, zoneLetter = utm.from_latlon(latitude, longitude)
-  except:
-    print("skipping: ", longitude, latitude)
-    continue
 
   parsedObs['latitude'] = latitude
   parsedObs['longitude'] = longitude
 
   parsedData.append(parsedObs)
 
-df = pd.DataFrame(parsedData)
-df.to_csv('data.csv')
-<<<<<<< HEAD
-
-with open('parsedData.json', 'w') as ff:
+with open('processed/mosquito_data.json', 'w') as ff:
   json.dump(parsedData, ff)
 
-=======
->>>>>>> 83c3b0ad962f00ef272d3bb40c8eb662db7b361e

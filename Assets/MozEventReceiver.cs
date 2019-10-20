3787d3e81ea2550c675f70cd57d0ffef60ca4ddc
Not Humpheh <trymunx@gmail.com>
2019-10-20 11:48:57 +0100

    Make grow_time depend on the event timer

--- Assets/MozEventReceiver.cs
+++ Assets/MozEventReceiver.cs
@@ -13,7 +13,8 @@ public class MozEventReceiver : MonoBehaviour
         GameObject newObject = Instantiate(eventPrefab, pos, Quaternion.identity);
         newObject.transform.parent = transform;
         newObject.GetComponent<Outbreak>().eventType = evt.eventType;
-        Debug.Log(evt.eventType);
+        newObject.GetComponent<Outbreak>().evt = evt;
+        // Debug.Log(evt.eventType);
 
         Debug.Log(evt.imageURL);
         // Debug.Log(evt.location.latitude);

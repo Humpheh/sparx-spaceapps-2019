d36bbc12b3f17929f7df5cc4b565f97b49726dc5
Not Humpheh <trymunx@gmail.com>
2019-10-20 05:49:01 +0100

    draw dot

--- Assets/MozEventReceiver.cs
+++ Assets/MozEventReceiver.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class MozEventReceiver : MonoBehaviour
 {
+    public GameObject eventPrefab;
     public void MozEvent(MozEvent evt)
     {
         Debug.Log(evt);
+        Vector3 pos = Utils.LatLongToMapCoords(-(int)evt.location.latitude, (int)evt.location.longitude);
+        GameObject newObject = Instantiate(eventPrefab, pos, Quaternion.identity);
+        newObject.transform.parent = transform;
     }
 }
